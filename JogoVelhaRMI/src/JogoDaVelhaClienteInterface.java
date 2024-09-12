5a1ddc8504188d13e2017793f4d00d3421d01754
Vitória Melo <95388328+vitoriameloo0@users.noreply.github.com>
2024-09-12 09:52:51 -0300

    nova versao

--- JogoVelhaRMI/src/JogoDaVelhaClienteInterface.java
+++ JogoVelhaRMI/src/JogoDaVelhaClienteInterface.java
@@ -2,11 +2,27 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface JogoDaVelhaClienteInterface extends Remote {
-    void atualizarTabuleiro(int index, String simbolo) throws RemoteException;
 
-    void atualizarPlacar(int pontosX, int pontosO) throws RemoteException;
+    public void autorizarJogada(JogoDaVelhaServidorInterface servidor) throws RemoteException;
 
-    void notificarVitoria(String vencedor) throws RemoteException;
+    public void finalizarJogo() throws RemoteException;
 
-    void notificarSuaVez() throws RemoteException;
+    public void getRespostaServidor(String resposta) throws RemoteException;
+
+    public void getJogadaAdversario(int idAdversario, int linha, int coluna) throws RemoteException;
+
+    public void setIdJogador(int id) throws RemoteException;
+
+    public void finalizarProcessoCliente() throws RemoteException;
+
+    public void setTelaJogador(TelaJogador tela) throws RemoteException;
+    
+    public void resetaTela() throws RemoteException;
+    
+    public void setServidor(JogoDaVelhaServidorInterface server) throws RemoteException;
+    
+    public void criarTela(int id) throws RemoteException;
+    
+    public void setPlacar(String placar) throws RemoteException;
+    
 }
