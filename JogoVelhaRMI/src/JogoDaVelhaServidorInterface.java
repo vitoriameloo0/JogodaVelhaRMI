2f65436081297419c6ab05e7c7d47d92cf1ab538
Vitória Melo <95388328+vitoriameloo0@users.noreply.github.com>
2024-09-12 09:38:53 -0300

    Create JogoDaVelhaServidorInterface.java

---
+++ JogoVelhaRMI/src/JogoDaVelhaServidorInterface.java
@@ -0,0 +1,12 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface JogoDaVelhaServidorInterface extends Remote {
+    void registrarJogador(JogoDaVelhaClienteInterface cliente, String simbolo) throws RemoteException;
+
+    void fazerJogada(int index, String simbolo) throws RemoteException;
+
+    void reiniciarJogo() throws RemoteException;
+
+    void zerarPlacar() throws RemoteException;
+}
